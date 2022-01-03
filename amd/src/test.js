2540eeabce3fe0312f5d0776b8095d9c7eada209
Paola Maneggia <paola.maneggia@isb.bayern.de>
2022-01-03 16:12:49 +0100

    Display nodes with d3-force.

--- amd/src/test.js
+++ amd/src/test.js
@@ -1,16 +1,37 @@
 export const init = (content) => {
-    window.paola = content;
-    console.log(content);
-    d3.select('svg').selectAll('circle').data(Object.keys(content)).enter().append('circle').attr('r', 10).attr('cx', () => Math.floor(Math.random() * 200)).attr('cy', () => Math.floor(Math.random() * 100))
-    console.log('Nodes: ' + nodes(content));
-    console.log('Edges: ');
-    window.simulation = forceSimulation(nodes(content));
+    let simulation = generateSimulation(content);
+    display(simulation);
 };
 
+function generateSimulation(dependencies) {
+    return d3.forceSimulation(nodes(dependencies))
+        .force('charge', d3.forceManyBody())
+        .force('link', d3.forceLink(edges(dependencies)).id(d => d.id))
+        .force('center', d3.forceCenter());
+}
+
+// Compute the nodes for d3-force as an array of objects {id: cm_id}
 function nodes(dependencies) {
-    return Object.keys(dependencies);
+    return Object.keys(dependencies).map(x => {return {id: x}});
 }
 
-function edges(dependencies) {
-
+// Compute the edges (links) for d3-force.
+function edges(data) {
+    return Object.entries(data).filter(([key, value]) => value !== null)
+        .flatMap(([key, value]) => {return value.c.map(x => {return {target: key, source: x.cm + ''}})});
 }
+
+function display(simulation) {
+    displayNodes(simulation.nodes());
+}
+
+function displayNodes(nodes) {
+    d3.select('svg').selectAll('circle').data(nodes)
+        .enter().append('circle')
+        .attr('r', 5)
+        .attr('cx', n => n.x * 5 + 100)
+        .attr('cy', n => n.y * 5 + 100);
+}
+
+
+
